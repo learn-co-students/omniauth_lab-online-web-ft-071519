7f6b80dd673042806f843c803d6e8154906a4fa6
Nicole Dow <nikki.dow71@gmail.com>
2019-10-03 09:59:31 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,16 @@
 class SessionsController < ApplicationController
+
+    def new
+    end
+
+    def create
+     auth = request.env['omniauth.auth']
+        #binding.pry
+      @user = User.find_or_create_by(uid: auth['uid']) do |u|
+        u.name = auth['info']['name']
+      end
+
+      session[:user_id] = @user.id 
+      render '/welcome/home'
+    end
 end
